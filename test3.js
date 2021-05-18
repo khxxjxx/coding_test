8bc7d5749164df2003cd58447e9e6f91dcdecb3d
김희진 <khxxjxx@naver.com>
2021-05-18 18:00:19 +0900

    2016년 요일찾기

--- test3.js
+++ test3.js
@@ -1,3 +1,5 @@
+// 2016년 요일 반환하기
+
 function solution(a, b) {
   let answer = '';
   let result = ['THU', 'FRI', 'SAT', 'SUN', 'MON', 'TUE', 'WED'];
@@ -28,31 +30,3 @@ let a = 3;
 let b = 3;
 
 console.log(solution(a, b));
-
-// ----
-
-// let result = ['THU', 'FRI', 'SAT', 'SUN', 'MON', 'TUE', 'WED'];
-// let answer = day(month) % 7;
-// console.log(result[answer]);
-
-// function day(month) {
-//   let a = 3;
-//   let b = 3;
-//   for (i = 1; i < a; i++) {
-//     b += month(i);
-//   }
-//   return b;
-// }
-
-// function month(z) {
-//   if ((z < 8 && z % 2 === 1) || (z > 7 && z % 2 == 0)) {
-//     z = 31;
-//   } else if (z === 2) {
-//     z = 29;
-//   } else {
-//     z = 30;
-//   }
-//   return z;
-// }
-
-// console.log(day(month));
