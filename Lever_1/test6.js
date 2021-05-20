a6392b8b7b6bf506b24e0936cd0688953b9693a7
Heejin <khxxjxx@naver.com>
2021-05-21 01:21:57 +0900

    나누어 떨어지는 숫자배열

---
+++ Lever_1/test6.js
@@ -0,0 +1,15 @@
+// 나누어 떨어지는 숫자배열
+
+function solution(arr, divisor) {
+  const answer = arr
+    .filter(number => number % divisor === 0)
+    .sort((a, b) => a - b);
+  answer == '' && answer.push(-1);
+  return answer;
+}
+
+// 아래 코드는 테스트를 위한 코드입니다.
+arr = [5, 9, 7, 10];
+divisor = 5;
+
+console.log(solution(arr, divisor));
