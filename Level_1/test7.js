3bdb08bc46c93e3b14c060ccb65efee3b62aa89f
Heejin <khxxjxx@naver.com>
2021-05-23 20:19:19 +0900

    평균 구하기

---
+++ Level_1/test7.js
@@ -0,0 +1,22 @@
+// 배열 arr에서 연속적으로 나타나는 숫자는 하나만 남기고 전부 제거
+
+/*
+// 예시
+const arr = [1, 1, 3, 3, 0, 1, 1];
+return [1, 3, 0, 1]
+*/
+
+function solution(arr) {
+  let answer = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] !== arr[i + 1]) {
+      answer.push(arr[i]);
+    }
+  }
+  return answer;
+}
+
+// 아래코드는 테스트를 위한 코드입니다.
+const arr = [1, 1, 3, 3, 0, 1, 1];
+
+console.log(solution(arr));
---
+++ Level_1/test9.js
@@ -0,0 +1,13 @@
+// git commit --amend --no-edit --date "Mon 20 Aug 2018 20:19:19 KST"
+// 평균구하기
+
+function solution2(arr) {
+  let answer = 0;
+  arr.forEach(number => (answer += number / arr.length));
+  return answer;
+}
+
+// 아래 코드는 테스트를 위한 코드입니다.
+const arr = [1, 2, 3, 4]; // 2.5
+
+console.log(solution2(arr));
