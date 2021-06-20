91f15c6deec3f72cc5f95a0d00aa2e26c7a83e71
Heejin <khxxjxx@naver.com>
2021-06-20 23:54:36 +0900

    최댓값과 최솟값

---
+++ Lever_2/test3.js
@@ -0,0 +1,10 @@
+// 최댓값과 최솟값
+
+let solution = s =>
+  `${Math.min(...s.split(' ')) + ''} ${Math.max(...s.split(' ')) + ''}`;
+
+// 아래코드는 테스트를 위한 코드입니다.
+const s = '-1 -2 -3 -4';
+// return "-4 -1"
+
+console.log(solution(s));
